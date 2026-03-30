47991a3c427b381128d36f8fc429389fa74f64d9
karthick151205 <karthickraja5753@gmail.com>
2026-03-30 14:59:49 +0530

    final

--- mini-lms/src/api/index.js
+++ mini-lms/src/api/index.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://mini-lms-course-progress-tracking-api.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://mini-lms-course-progress-tracking-api.onrender.com";
+//const BASE_URL = "http://localhost:5000";
 
 // ── Helpers ──────────────────────────────────────────────────────
 const getStoredUser = () => JSON.parse(localStorage.getItem("user"));
